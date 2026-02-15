33e549a5c593f70d3f82d0b8c073ace1c1506f75
Leonardogit <leonardoiglesias74@gmail.com>
2026-02-15 16:05:36 -0300

    import openz vs code

--- contract/Cryptonino.sol
+++ contract/Cryptonino.sol
@@ -1,15 +1,11 @@
-
 // SPDX-License-Identifier: LPSD-3.0
+
 pragma solidity 0.8.33;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Cryptonino is ERC20 {
-
     constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) { 
-        _mint(msg.sender, 9000000 * 1e18);
-
-
+        _mint(msg.sender, 9000000 * 1e18);  // 9,000,000 tokens (con 18 decimales)
     }
-
 }
